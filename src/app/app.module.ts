de5540f2f4f60f5bdef797ae8a2091013d83a290
Mark-Ball <markball111@gmail.com>
2020-04-07 10:42:24 +1000

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './components/pages/movies/movies.component';
+import { CinemasComponent } from './components/pages/cinemas/cinemas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    CinemasComponent
   ],
   imports: [
     BrowserModule,
