f90efcdc2f99707172105fd250bc5975a3616399
Mark-Ball <markball111@gmail.com>
2020-04-09 10:19:52 +1000

    created method to get screenings by cinema

--- src/app/components/pages/movie/movie.component.ts
+++ src/app/components/pages/movie/movie.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Movie } from '../../../models/Movie';
+import { Cinema } from '../../../models/Cinema';
 import { Screening } from '../../../models/Screening';
 import { MovieService } from '../../../services/movie.service';
+import { CinemaService } from '../../../services/cinema.service';
 import { ScreeningService } from '../../../services/screening.service';
 import { movies } from '../../../models/data';
 import { createImagePath } from '../../../services/utils';
@@ -16,11 +18,13 @@ export class MovieComponent implements OnInit {
   movie: Movie;
   id: number;
   imagePath: string;
-  screenings: Screening[];
+  cinemas: Cinema[];
+  screeningsByCinema: Array<{ cinemaId: number, cinemaName: string, screenings: Array<string> }>;
 
   constructor(
     private route: ActivatedRoute,
     private movieService: MovieService,
+    private cinemaService: CinemaService,
     private screeningService: ScreeningService
   ) { }
 
@@ -32,8 +36,23 @@ export class MovieComponent implements OnInit {
     // get movie based on id
     this.movie = this.movieService.getMovie(this.id);
     this.imagePath = createImagePath(this.movie.image);
-    this.screenings = this.screeningService.getScreenings(this.id, null);
-    console.log(this.screenings);
+    this.cinemas = this.cinemaService.getCinemas();
+
+    // loop through cinemas and get screenings at each
+    this.screeningsByCinema = this.getScreeningsByCinema();
+    console.log(this.screeningsByCinema);
+  }
+
+  getScreeningsByCinema(): Array<{ cinemaId: number, cinemaName: string, screenings: Array<string> }> {
+    const result = [];
+    for (let i = 1; i <= this.cinemas.length; i++) {
+      result.push({
+        cinemaId: i,
+        cinemaName: this.cinemas[i],
+        screenings: this.screeningService.getScreenings(this.id, i)
+      })
+    }
+    return result;
   }
 
 }
--- src/app/services/screening.service.ts
+++ src/app/services/screening.service.ts
@@ -11,12 +11,17 @@ export class ScreeningService {
   constructor() { }
 
   getScreenings(movId: number, cinId: number): Screening[] {
-    // if a movieId is provided, filter based on movieId
-    if (movId) {
+    // if no movId provided, filter based on cinId
+    if (!movId) {
+      return this.screenings.filter(screening => screening.cinemaId === cinId);
+    }
+    // if no cinId provided, filter based on movId
+    if (!cinId) {
       return this.screenings.filter(screening => screening.movieId === movId);
     }
-    // if a cinemaId is provided, filter based on cinemaId
-    return this.screenings.filter(screening => screening.cinemaId === cinId);
-    
+    // if both ids provided, filter based on both
+    return this.screenings.filter(screening => (
+      screening.movieId === movId && screening.cinemaId == cinId
+    ))
   }
 }
