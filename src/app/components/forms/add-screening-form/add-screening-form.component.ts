749811844db2f2dd919922928d0f93868398a330
Mark-Ball <markball111@gmail.com>
2020-04-10 17:57:53 +1000

    add-screening-form now detects which movie or cinema page it is on and automatically enters that info

--- src/app/components/forms/add-screening-form/add-screening-form.component.ts
+++ src/app/components/forms/add-screening-form/add-screening-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Movie } from '../../../models/Movie';
 import { Cinema } from '../../../models/Cinema';
 import { Screening } from '../../../models/Screening';
@@ -15,6 +15,8 @@ export class AddScreeningFormComponent implements OnInit {
   movies: Movie[];
   cinemas: Cinema[];
   screening = new Screening(null, null, null, null);
+  @Input() renderedInsideOf: string;
+  @Input() resourceId: number;
   @Output() updater = new EventEmitter();
 
   constructor(
@@ -29,13 +31,15 @@ export class AddScreeningFormComponent implements OnInit {
   }
 
   onSubmit(): void {
-    const { movieId, cinemaId, time } = this.screening;
+    let { movieId, cinemaId, time } = this.screening;
+    if (this.renderedInsideOf === 'movie') {
+      movieId = this.resourceId;
+    }
+    if (this.renderedInsideOf == 'cinema') {
+      cinemaId = this.resourceId;
+    }
     this.screeningService.createScreening(+movieId, +cinemaId, time);
-    this.updater.emit();
-  }
-
-  get diagnostic() {
-    return JSON.stringify(this.screening);
+    this.updater.emit(); // causes parent to update screenings
   }
 
 }
